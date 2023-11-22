605ed195e1c6442c7a92b888e9ecd081a20c5663
Kikazuru <45072184+Kikazuru@users.noreply.github.com>
2023-11-22 11:34:35 +0700

    Add files via upload

---
+++ assets/EntryView-69b2ef60.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as r,c,a as n}from"./index-765dcdae.js";const s={},a={class:"d-flex justify-center align-center w-100 h-100"};function _(i,f){const e=o("router-view");return r(),c("div",a,[n(e)])}const d=t(s,[["render",_]]);export{d as default};
