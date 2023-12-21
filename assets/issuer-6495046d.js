c9278961a0b5d8053bc15e45675fd052a7832c1e
Kikazuru <45072184+Kikazuru@users.noreply.github.com>
2023-12-21 15:00:55 +0700

    Add files via upload

---
+++ assets/issuer-6495046d.js
@@ -0,0 +1,1 @@
+import{a}from"./api-f2d6da92.js";async function u({states:s,limit:e=10,page:t=1}){return await a.get("/issuers",{params:{states:s,limit:e,page:t}})}async function n(s){return await a.post("/issuers",s)}async function i(s){return await a.delete(`/issuers/${s}`)}async function c(s,e){return await a.put(`/issuers/${s}`,e)}export{n as c,i as d,u as l,c as u};
---
+++ assets/tag-13d67880.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-033028ba.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
